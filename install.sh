160a6d021b5ea2150038dbd89d903c34ad017f49
Derrick Weis <dweis@box.com>
2016-08-18 14:49:38 -0400

    minor fixes / osx

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -sf ${DIR}/dircolors ~/.dircolors
-ln -sf ${DIR}/git/gitconfig ~/.gitconfig
+#ln -sf ${DIR}/git/gitconfig ~/.gitconfig
 ln -sf ${DIR}/tmux/tmux.conf ~/.tmux.conf
 ln -sf ${DIR}/vim/vim ~/.vim
-ln -sf ${DIR}/vim/vimrc ~/.vimrc
+ln -sf ${DIR}/vim/vim/init.vim ~/.vimrc
 ln -sf ${DIR}/zsh/zshrc ~/.zshrc
