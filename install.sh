55649d49636adc6d75977619b09afed4d4288034
Derrick Weis <derrick@derrickweis.com>
2018-10-08 14:19:41 -0400

    add taffybar/termite/xmonad

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+mkdir -p ~/.config
 
 ln -s ${DIR}/dircolors ~/.dircolors
 ln -s ${DIR}/git/gitconfig ~/.gitconfig
@@ -9,3 +10,6 @@ ln -s ${DIR}/vim/vim ~/.vim
 ln -s ${DIR}/vim/vim/init.vim ~/.vimrc
 ln -s ${DIR}/zsh/zshrc ~/.zshrc
 ln -s ${DIR}/spacemacs/spacemacs ~/.spacemacs
+ln -s ${DIR}/xmonad ~/.xmonad
+ln -s ${DIR}/taffybar ~/.config/taffybar
+ln -s ${DIR}/termite ~/.config/termite
---
+++ xmonad/xmonad.hs
@@ -0,0 +1,28 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeysP)
+import System.IO
+
+main = do
+  xmproc <- spawnPipe "xmobar"
+  xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts $ layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50
+            }
+        , terminal = "termite" }
+        `additionalKeysP`
+              [ ("M-p", spawn "x=$(dmenu -i -fn Hack) && exec $x")
+              , ("M-q", spawn "xmonad --recompile && xmonad --restart")
+              , ("M-f", spawn "firefox")
+              , ("<XF86AudioRaiseVolume>", spawn "amixer sset Master 3%+")
+              , ("<XF86AudioLowerVolume>", spawn "amixer sset Master 3%-")
+              , ("<XF86AudioMute>",        spawn "amixer sset Master toggle")
+              , ("M-S-z", spawn "xscreensaver-command -lock; xset dpms force off")
+              , ("M-S-p", spawn "sleep 0.2; scrot -s")
+              , ("M-S-C-p", spawn "scrot")
+              ]
