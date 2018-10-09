ddd964b77c4487ae60d5b9dbf3e07f7e017b0c95
Derrick Weis <derrick@derrickweis.com>
2018-10-08 20:31:50 -0400

    cleanup

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -8,29 +8,23 @@ import XMonad.Util.EZConfig(additionalKeysP)
 import System.IO
 
 main = do
-  tbproc <- spawnPipe "taffybar"
-
-  xmonad $ 
+  xmonad $
     docks $
     ewmh $
     pagerHints defaultConfig
         { manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts $ layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-            { ppOutput = hPutStrLn tbproc
-            , ppTitle = xmobarColor "green" "" . shorten 50
-            }
         , terminal = "termite"
         , borderWidth = 0
         }
         `additionalKeysP`
-              [ ("M-p", spawn "x=$(dmenu -i -fn Hack) && exec $x")
-              , ("M-S-r", spawn "xmonad --recompile && xmonad --restart")
-              , ("M-S-f", spawn "firefox")
+              [ ("M-p",                    spawn "x=$(dmenu -i -fn Hack) && exec $x")
+              , ("M-S-r",                  spawn "xmonad --recompile && xmonad --restart")
+              , ("M-S-f",                  spawn "firefox")
               , ("<XF86AudioRaiseVolume>", spawn "amixer sset Master 3%+")
               , ("<XF86AudioLowerVolume>", spawn "amixer sset Master 3%-")
               , ("<XF86AudioMute>",        spawn "amixer sset Master toggle")
-              , ("M-S-z", spawn "xscreensaver-command -lock; xset dpms force off")
-              , ("M-S-p", spawn "sleep 0.2; scrot -s")
-              , ("M-S-C-p", spawn "scrot")
+              , ("M-S-z",                  spawn "xscreensaver-command -lock; xset dpms force off")
+              , ("M-S-p",                  spawn "sleep 0.2; scrot -s")
+              , ("M-S-C-p",                spawn "scrot")
               ]
